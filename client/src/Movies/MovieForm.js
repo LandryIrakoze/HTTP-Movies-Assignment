b70eb4e65dfd8edade7c68764f9a64cfff510200
Landry Irakoze <irakozel@gmail.com>
2019-08-22 18:18:13 -0400

    fixed form

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 const MovieForm = props => {
 
     console.log('formprops', props);
+    // console.log('form id', props.match.params.id);
     const [movieInfo, setMovieInfo] = useState({title: '', director: '', metascore: '', stars: []});
-
     // const [stars, setStars] = useState([]);
 
 
@@ -13,19 +13,23 @@ const MovieForm = props => {
         setMovieInfo({...movieInfo, [event.target.name]: event.target.value })
     }
 
-    const handleStars = event => {
-        setMovieInfo({...movieInfo, [movieInfo.stars]: [...event.target.value]})
+    const handleStars = (event, i) => {
+        console.log(event.target.value);
+        const arr = [...movieInfo.stars];
+        arr[i] = event.target.value;
+        setMovieInfo({...movieInfo, stars: arr});
     }
 
     const handleSubmit = event => {
+        event.preventDefault();
+        axios.put(`http://localhost:5000/api/movies/${props.match.params.id}/`, movieInfo)
+            .then(res => {
+                console.log('put response', res)
+                //push history from here
+            })
+            .catch(err => console.error('put error', err))
         // event.preventDefault();
-        // axios.put(`http://localhost:5000/api/movies/${props.match.params.id}/`, movieInfo)
-        //     .then(res => {
-        //         console.log('put response', res)
-        //         //push history from here
-        //     })
-        //     .catch(err => console.error('put error', err))
-        console.log('movieinfo', movieInfo);
+        // console.log('movieinfo', movieInfo);
     }
 
     return (
@@ -40,20 +44,20 @@ const MovieForm = props => {
                     <input type="text" name="director" value={movieInfo.director} onChange={event => handleChange(event)} />
                 </label>
                 <label>
-                    Metascore
+                    Metascore:
                     <input type="number" name="metascore" value={movieInfo.metascore} onChange={event => handleChange(event)} />
                 </label>
                 <label>
                     Stars:
-                    <input type="text" name="stars" value={movieInfo.stars} onChange={event => handleChange(event)} />
+                    <input type="text" name="stars" value={movieInfo.stars[0]} onChange={event => handleStars(event, 0)} />
                 </label>
                 <label>
                     Stars:
-                    <input type="text" name="stars" value={movieInfo.stars} onChange={event => handleChange(event)} />
+                    <input type="text" name="stars" value={movieInfo.stars[1]} onChange={event => handleStars(event, 1)} />
                 </label>
                 <label>
                     Stars:
-                    <input type="text" name="stars" value={movieInfo.stars} onChange={event => handleChange(event)} />
+                    <input type="text" name="stars" value={movieInfo.stars[2]} onChange={event => handleStars(event, 2)} />
                 </label>
                 <button>Submit</button>
             </form>
